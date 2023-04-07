08a54b230ebeaef7b90d35aa4d9a3f17350170ec
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-04-07 12:10:10 -0500

    Add react toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { CartContextProvider } from "@/context/Cart";
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "@/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CartContextProvider>
       <Component {...pageProps} />
+      <ToastContainer position="bottom-right"/>
     </CartContextProvider>
   );
 }

5b390617b167845c27aa56027b8161d2c5403fe4
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-04-06 20:28:00 -0500

    Add function to format number to COP

---
+++ src/utils/format.ts
@@ -0,0 +1,7 @@
+export function numberToPrice(value: number): string {
+  return value.toLocaleString("es-CO", {
+    style: "currency",
+    currency: "COP",
+    maximumFractionDigits: 0,
+  });
+}

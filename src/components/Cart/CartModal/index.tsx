8df58395096348bd17ed4c4988fbeec5459cbdde
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-04-08 23:35:33 -0500

    Add relative price according to quantity

--- src/components/Cart/CartModal/index.tsx
+++ src/components/Cart/CartModal/index.tsx
@@ -26,7 +26,7 @@ export default function CartModal({ show, setShow }: CartModalProps) {
         ref={modalRef}
         className={`justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ${animation["fade-in-down"]}`}
       >
-        <div className="relative w-auto mx-auto max-w-3xl">
+        <div className="relative w-auto mx-auto max-w-4xl">
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             <CartModalHeader handleCloseModal={handleCloseModal} />
             <CartModalBody />

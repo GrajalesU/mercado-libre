da196c83004aa0d1588fb864e2007d6316c39447
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-04-12 15:19:05 -0500

    Revert Benefits commit

--- src/components/Benefits/BenefitCard.tsx
+++ src/components/Benefits/BenefitCard.tsx
@@ -9,6 +9,7 @@ type BenefitCardProps = {
   brand: string;
   title: string;
   freeTier?: boolean;
+  gradientColor?: string;
 };
 
 const BenefitCard = ({
@@ -19,8 +20,9 @@ const BenefitCard = ({
   brand,
   title,
   freeTier = false,
+  gradientColor = 'rgba(0, 0, 0, 0.5)',
 }: BenefitCardProps) => (
-  <article className='relative h-[250px] w-[384px] overflow-hidden rounded-md'>
+  <article className='relative aspect-video h-[184px] overflow-hidden rounded-md lg:h-[250px] lg:w-[384px]'>
     <Image
       src={bgSrc}
       alt={bgAlt}
@@ -28,9 +30,20 @@ const BenefitCard = ({
       height={250}
       className='absolute object-cover'
     />
-    <div className='absolute bottom-4 left-4 z-10 flex items-center gap-4'>
-      <Image src={logoSrc} width={80} height={80} alt={logoAlt} />
-      <div className='text-white'>
+    <div
+      style={{
+        background: `linear-gradient(0deg, ${gradientColor} 0%, rgba(0, 0, 0, 0) 100%)`,
+      }}
+      className='absolute bottom-0 left-0 right-0 z-10 flex flex-col items-center gap-0 lg:mt-8 lg:flex-row lg:gap-4 lg:pb-4 lg:pl-4 '
+    >
+      <Image
+        src={logoSrc}
+        width={80}
+        height={80}
+        alt={logoAlt}
+        className='h-12 w-12 lg:h-20 lg:w-20'
+      />
+      <div className='mt-3 mb-2 text-center text-white lg:mt-0 lg:mb-0 lg:text-left'>
         {freeTier && <span className='text-[11px]'>7 DÍAS GRATIS</span>}
         <h3 className='font-semibold'>{title}</h3>
         <span className='font-[17px]'>{brand}</span>
--- src/components/Benefits/index.tsx
+++ src/components/Benefits/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import BenefitCard from './BenefitCard';
 
 const Benefits = () => (
-  <section className='mb-20'>
-    <div className='mb-[22px] flex items-end gap-[16.38px]'>
-      <h2 className='text-[25px] font-light text-gray-300'>
+  <section className='mb-20 h-full rounded bg-white pb-1 shadow-sm shadow-gray-100 lg:bg-transparent lg:shadow-none'>
+    <header className='mb-[22px] flex items-baseline gap-[16.38px] border-b border-gray-100 px-4 lg:border-none lg:px-0'>
+      <h2 className='my-3 text-lg font-bold text-black lg:my-0 lg:text-[25px] lg:font-light lg:leading-6 lg:text-gray-300'>
         Beneficios de Mercado Puntos
       </h2>
-      <span className='mb-[7px] cursor-pointer text-[15px] font-normal leading-5 text-blue'>
+      <span className='mb-[7px] hidden cursor-pointer text-[15px] font-normal leading-5 text-blue lg:inline'>
         Ver todos los beneficios
       </span>
-    </div>
-    <ul className='flex justify-between'>
+    </header>
+    <ul className='flex justify-between overflow-hidden'>
       <li>
         <BenefitCard
           bgSrc='/benefits/DisneyAndStarBg.png'
@@ -20,6 +20,7 @@ const Benefits = () => (
           logoAlt='Disney Plus and Star Plus Logo'
           title='Sin cargo con el nivel 6'
           brand='Disney+ y Star+'
+          gradientColor='#081330'
         />
       </li>
       <li>
@@ -30,6 +31,7 @@ const Benefits = () => (
           logoAlt='HBO Max Logo'
           title='Hasta 50% OFF'
           brand='HBO Max'
+          gradientColor='#3C034E'
           freeTier
         />
       </li>
@@ -41,10 +43,27 @@ const Benefits = () => (
           logoAlt='Paramount Logo'
           title='Hasta 50% OFF'
           brand='Paramount+'
+          gradientColor='#0064FF'
           freeTier
         />
       </li>
     </ul>
+
+    <footer className='mt-[22px] flex items-center justify-between border-t border-gray-100 px-3 py-4 text-sm font-medium text-blue lg:hidden'>
+      <span>Ver todos los beneficios</span>
+      <svg
+        className='h-3 w-2 stroke-blue'
+        viewBox='0 0 9 14'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          fill='none'
+          fill-rule='evenodd'
+          stroke-width='1.5'
+          d='M1 1.343L6.657 7 1 12.657'
+        ></path>
+      </svg>
+    </footer>
   </section>
 );
 

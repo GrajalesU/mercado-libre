a496f932618eb9edf2246c8ee0346fed96520cf0
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-04-12 14:48:27 -0500

    Add responsive design

--- src/components/Benefits/BenefitCard.tsx
+++ src/components/Benefits/BenefitCard.tsx
@@ -9,34 +9,47 @@ type BenefitCardProps = {
   brand: string;
   title: string;
   freeTier?: boolean;
+  gradientColor?: string;
 };
 
-export default function BenefitCard({
+const BenefitCard = ({
   bgSrc,
   bgAlt,
   logoSrc,
   logoAlt,
   brand,
   title,
-  freeTier,
-}: BenefitCardProps) {
-  return (
-    <article className="w-[384px] h-[250px] rounded-md overflow-hidden relative">
+  freeTier = false,
+  gradientColor = "#000",
+}: BenefitCardProps) => (
+  <article className="h-[184px] lg:w-[384px] lg:h-[250px] aspect-video rounded-md overflow-hidden relative">
+    <Image
+      src={bgSrc}
+      alt={bgAlt}
+      width={384}
+      height={250}
+      className="absolute object-cover"
+    />
+    <div
+      style={{
+        background: `linear-gradient(0deg, ${gradientColor} 0%, rgba(0, 0, 0, 0) 100%)`,
+      }}
+      className="absolute bottom-0 left-0 right-0 z-10 flex flex-col items-center gap-0 lg:flex-row lg:gap-4 lg:mt-8 lg:pb-4 lg:pl-4 "
+    >
       <Image
-        src={bgSrc}
-        alt={bgAlt}
-        width={384}
-        height={250}
-        className="absolute object-cover"
+        src={logoSrc}
+        width={80}
+        height={80}
+        alt={logoAlt}
+        className="w-12 h-12 lg:w-20 lg:h-20"
       />
-      <div className="absolute z-10 flex items-center gap-4 bottom-4 left-4">
-        <Image src={logoSrc} width={80} height={80} alt={logoAlt} />
-        <div className="text-white">
-          {freeTier && <span className="text-[11px]">7 DÍAS GRATIS</span>}
-          <h3 className="font-semibold">{title}</h3>
-          <span className="font-[17px]">{brand}</span>
-        </div>
+      <div className="mt-3 mb-2 text-center text-white lg:mt-0 lg:mb-0 lg:text-left">
+        {freeTier && <span className="text-[11px]">7 DÍAS GRATIS</span>}
+        <h3 className="font-semibold">{title}</h3>
+        <span className="font-[17px]">{brand}</span>
       </div>
-    </article>
-  );
-}
+    </div>
+  </article>
+);
+
+export default BenefitCard;
--- src/components/Benefits/index.tsx
+++ src/components/Benefits/index.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import BenefitCard from "./BenefitCard";
 
-export default function Benefits() {
-  return (
-    <section className="mb-20">
-      <div className="flex gap-[16.38px] items-end mb-[22px]">
-        <h2 className="text-gray-300 text-[25px] font-light">
+const Benefits = () => (
+    <section className="h-full pb-1 mb-20 bg-white rounded shadow-sm shadow-gray-100 lg:shadow-none lg:bg-transparent">
+      <header className="flex gap-[16.38px] items-baseline mb-[22px] border-b lg:border-none border-gray-100 px-4 lg:px-0">
+        <h2 className="text-black lg:text-gray-300 text-lg lg:text-[25px] lg:leading-6 font-bold lg:font-light my-3 lg:my-0">
           Beneficios de Mercado Puntos
         </h2>
-        <span className="font-normal text-blue text-[15px] leading-5 mb-[7px] cursor-pointer">
+        <span className="font-normal text-blue text-[15px] leading-5 mb-[7px] cursor-pointer hidden lg:inline">
           Ver todos los beneficios
         </span>
-      </div>
-      <ul className="flex justify-between">
+      </header>
+      <ul className="flex justify-between overflow-hidden">
         <li>
           <BenefitCard
             bgSrc="/benefits/DisneyAndStarBg.png"
@@ -21,6 +20,7 @@ export default function Benefits() {
             logoAlt="Disney Plus and Star Plus Logo"
             title="Sin cargo con el nivel 6"
             brand="Disney+ y Star+"
+            gradientColor="#081330"
           />
         </li>
         <li>
@@ -31,6 +31,7 @@ export default function Benefits() {
             logoAlt="HBO Max Logo"
             title="Hasta 50% OFF"
             brand="HBO Max"
+            gradientColor="#3C034E"
             freeTier
           />
         </li>
@@ -42,10 +43,28 @@ export default function Benefits() {
             logoAlt="Paramount Logo"
             title="Hasta 50% OFF"
             brand="Paramount+"
+            gradientColor="#0064FF"
             freeTier
           />
         </li>
       </ul>
+
+      <footer className="px-3 py-4 border-t border-gray-100 mt-[22px] text-blue font-medium text-sm flex justify-between items-center lg:hidden">
+        <span>Ver todos los beneficios</span>
+        <svg
+          className="w-2 h-3 stroke-blue"
+          viewBox="0 0 9 14"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="none"
+            fill-rule="evenodd"
+            stroke-width="1.5"
+            d="M1 1.343L6.657 7 1 12.657"
+          ></path>
+        </svg>
+      </footer>
     </section>
   );
-}
+
+  export default Benefits;

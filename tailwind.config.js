1eb366444bb3a81d780e957d1b6dc9e770771cd0
mariarua <maria.rua1@udea.edu.co>
2023-04-19 14:23:21 -0500

    Add custom shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     colors: {
-      transparent: "transparent",
-      yellow: "#FFF159",
+      transparent: 'transparent',
+      yellow: '#FFF159',
       gray: {
-        100: "#EBEBEB",
-        200: "#999999",
-        300: "#666666",
-        400: "#4B4B4B",
+        100: '#EBEBEB',
+        200: '#999999',
+        300: '#666666',
+        400: '#4B4B4B',
       },
-      black: "#333333",
-      blue: "#3483FA",
-      green: "#00A650",
-      white: "#FFFFFF",
-      red: "#FF0000",
+      black: '#333333',
+      blue: '#3483FA',
+      green: '#00A650',
+      white: '#FFFFFF',
+      red: '#FF0000',
+    },
+    boxShadow: {
+      narrow: '0 2px 4px 0 rgba(0,0,0,.19)',
+      arrow: '0 7px 16px 0 rgba(0, 0, 0, 0.2), 0 1px 3px 0 rgba(0, 0, 0, 0.1)',
     },
     extend: {},
   },

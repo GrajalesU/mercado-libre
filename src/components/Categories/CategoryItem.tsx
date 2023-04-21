4120cc2fe78a35db80dff6fe84ceae9f8b956571
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2023-04-20 19:22:13 -0500

    Responsive categories

--- src/components/Categories/CategoryItem.tsx
+++ src/components/Categories/CategoryItem.tsx
@@ -8,7 +8,7 @@ type CategoryItemProps = {
 };
 
 export const CategoryItem = ({ src, alt, description }: CategoryItemProps) => (
-  <div className='flex h-40 w-full flex-col items-center gap-4 border border-gray-100 bg-white'>
+  <div className='rounded-md lg:rounded-none flex h-40 w-full flex-col items-center gap-4 border border-gray-100 bg-white'>
     <Image src={src} alt={alt} width={48} height={48} className='mt-[30px]' />
     <span className='text-center text-black'>{description}</span>
   </div>

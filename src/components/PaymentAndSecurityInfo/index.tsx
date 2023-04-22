b0dd0ce96d7919a619d07f8a2e80e380fea548a1
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-04-22 09:44:27 -0500

    Add responsive design

--- src/components/PaymentAndSecurityInfo/index.tsx
+++ src/components/PaymentAndSecurityInfo/index.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 export const PaymentAndSecurityInfo = () => (
   <section className='py-10'>
-    <ul className='flex items-start justify-center'>
-      <li className='grow'>
+    <ul className='flex flex-col items-stretch justify-center lg:flex-row lg:items-start'>
+      <li className='mb-4 grow'>
         <div className='flex flex-col items-center text-center'>
           <Image
             src='/Payment.svg'
@@ -16,7 +16,7 @@ export const PaymentAndSecurityInfo = () => (
           <h3 className='text-xl font-medium text-gray-400'>
             Paga con tarjeta o en efectivo
           </h3>
-          <p className='mt-1 max-w-[300px] text-[15px] text-gray-200'>
+          <p className='mt-1 px-10 lg:px-0 lg:max-w-[300px] text-[15px] text-gray-200'>
             Con Mercado Pago, paga en cuotas y aprovecha la comodidad de
             financiación que te da tu banco, o hazlo con efectivo en puntos de
             pago. ¡Y siempre es seguro!
@@ -26,8 +26,8 @@ export const PaymentAndSecurityInfo = () => (
           </span>
         </div>
       </li>
-      <div className='h-[65px] w-1 self-center border-l border-l-gray-100' />
-      <li className='grow'>
+      <div className='self-center w-full border-b border-b-gray-100 lg:rotate-90 lg:w-[65px]' />
+      <li className='my-4 grow'>
         <div className='flex flex-col items-center text-center'>
           <Image
             src='/Shipping.svg'
@@ -39,7 +39,7 @@ export const PaymentAndSecurityInfo = () => (
           <h3 className='text-xl font-medium text-gray-400'>
             Envío gratis desde $ 90.000
           </h3>
-          <p className='mt-1 max-w-[300px] text-[15px] text-gray-200'>
+          <p className='mt-1 px-10 lg:px-0 lg:max-w-[300px] text-[15px] text-gray-200'>
             Con solo estar registrado en Mercado Libre, tienes envíos gratis en
             miles de productos seleccionados.
           </p>
@@ -48,8 +48,8 @@ export const PaymentAndSecurityInfo = () => (
           </span>
         </div>
       </li>
-      <div className='h-[65px] w-1 self-center border-l border-l-gray-100' />
-      <li className='grow'>
+      <div className='self-center w-full border-b border-b-gray-100 lg:rotate-90 lg:w-[65px]' />
+      <li className='mt-4 grow'>
         <div className='flex flex-col items-center text-center'>
           <Image
             src='/Protected.svg'
@@ -61,7 +61,7 @@ export const PaymentAndSecurityInfo = () => (
           <h3 className='text-xl font-medium text-gray-400'>
             Seguridad, de principio a fin
           </h3>
-          <p className='mt-1 max-w-[300px] text-[15px] text-gray-200'>
+          <p className='mt-1 px-10 lg:px-0 lg:max-w-[300px] text-[15px] text-gray-200'>
             ¿No te gusta? ¡Devuélvelo! En Mercado Libre, no hay nada que no
             puedas hacer, porque estás siempre protegido.
           </p>
